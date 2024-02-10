c301585b8c317d291faa327bd2902ea1d33de06f
matildejb <116381788+matildejb@users.noreply.github.com>
2024-02-10 23:00:35 +0100

    Añadiendo Lógica
    Añadir form a la lista de noticias

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.css'
 })
 export class BlogComponent {
+  /*Array multidimensional de json, donde cada elemento del array sea una publicacion y lo iniciaremos 
+  con nuestras dos noticias */
+
+arraPublicaciones: any [] = [
+ /* {'titulo': 'Noticia', 'contenido': 'Contenido Noticia','fecha': '01.02.22', 'img': 'imagen'}*/
+]
+
+newPublicacion: any = {
+  'titulo': "", 
+  'contenido': "",
+  'fecha': "", 
+  'img': ""
+}
+
+guardarDatos(){
+  this.arraPublicaciones.push(this.newPublicacion);
+  this.newPublicacion = {
+    'titulo': "", 
+    'contenido': "",
+    'fecha': "", 
+    'img': ""
+  }
+
+}
+
+cargarDatos() : string {
+  let html: string = "";
+  this.arraPublicaciones.forEach( (element: any) => {
+    html += ` <p>${element.titulo}</p>  <p>${element.contenido}</p>  
+    <p>${element.fecha}</p> <p>${element.img}</p>  `
+  })
+  return html
+}
 
 }
