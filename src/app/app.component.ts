5088ce12c2ebdfa0a26a25f5690126dba9113ce4
matildejb <matildejimenezbedmar@gmail.com>
2024-02-12 13:06:05 +0100

    Formulario Validado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BlogComponent } from './components/blog/blog.component';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.css'
 })
+
 export class BlogComponent {
+  guardadoExitoso: boolean = false; 
   /*Array multidimensional de json, donde cada elemento del array sea una publicacion y lo iniciaremos 
   con nuestras dos noticias */
 
@@ -23,8 +27,19 @@ newPublicacion: any = {
   'img': ""
 }
 
+
 guardarDatos(){
+ 
+   // Verificar si alguno de los campos está vacío
+   if (!this.newPublicacion.titulo || !this.newPublicacion.contenido || !this.newPublicacion.fecha){
+    alert('Por favor, completa todos los campos del formulario.');
+    return; // Detener el proceso de guardar si hay campos vacíos
+  }
+  // Si todos los campos están completos, agregar la nueva publicación al arreglo
   this.arraPublicaciones.push(this.newPublicacion);
+  alert('¡Tu noticia se ha guardado correctamente!');
+  // Reiniciar el objeto newPublicacion para limpiar los campos del formulario
+
   this.newPublicacion = {
     'titulo': "", 
     'contenido': "",
